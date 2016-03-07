d4266ffe7ffed8eac930ba9b47b32a337c2ea68e
sorrow17 <sorrow17@hotmail.com>
2016-03-07 05:56:25 -0500

    migrate to maven

--- tsqlparser/src/com/relationalcloud/tsqlparser/exception/MatchingException.java
+++
@@ -1,9 +0,0 @@
-package com.relationalcloud.tsqlparser.exception;
-
-public class MatchingException extends NotImplementedException {
-
-	public MatchingException(String string) {
-		super(string);	
-	}
-
-}
